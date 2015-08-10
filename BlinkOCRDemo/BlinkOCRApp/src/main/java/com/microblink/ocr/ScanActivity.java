57e58599e468a5a6dfb7785dfdef44f1f00361b1
Nenad Mikša <nenad.miksa@microblink.com>
2015-08-10 12:46:30 +0200

    updated demo app for BlinkOCR SDK v1.6.0

--- BlinkOCRDemo/BlinkOCRApp/src/main/java/com/microblink/ocr/ScanActivity.java
+++ BlinkOCRDemo/BlinkOCRApp/src/main/java/com/microblink/ocr/ScanActivity.java
@@ -5,15 +5,14 @@ import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.res.Configuration;
-import android.database.DataSetObserver;
 import android.graphics.Rect;
 import android.os.Bundle;
-import android.view.Display;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.ImageButton;
-import android.widget.ListAdapter;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -33,12 +32,6 @@ import com.microblink.view.recognition.RecognitionType;
 import com.microblink.view.recognition.RecognizerView;
 import com.microblink.view.recognition.ScanResultListener;
 
-import java.util.HashSet;
-import java.util.Set;
-
-import it.sephiroth.android.library.widget.AdapterView;
-import it.sephiroth.android.library.widget.HListView;
-
 
 public class ScanActivity extends Activity implements CameraEventsListener, ScanResultListener {
 
@@ -48,8 +41,7 @@ public class ScanActivity extends Activity implements CameraEventsListener, Scan
     private EditText mResult;
     private boolean mTorchOn = false;
     private TextView mMessage;
-    private HListView mTitleIndicator;
-    private ScanConfigurationListAdapter mTitleAdapter;
+    private SlidingTabLayout mTitleIndicator;
     private ScanConfiguration[] mConfiguration = Configurator.createScanConfigurations();
     private int mSelectedConfiguration = 0;
 
@@ -68,20 +60,31 @@ public class ScanActivity extends Activity implements CameraEventsListener, Scan
         mResultView = findViewById(R.id.layResult);
         mMessage = (TextView) findViewById(R.id.txtMessage);
         mResult = (EditText) findViewById(R.id.txtResult);
-        mTitleIndicator = (HListView) findViewById(R.id.indicator);
+        mTitleIndicator = (SlidingTabLayout) findViewById(R.id.indicator);
 
-        mTitleAdapter = new ScanConfigurationListAdapter();
-        mTitleIndicator.setAdapter(mTitleAdapter);
+        ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
+        viewPager.setAdapter(new SamplePagerAdapter());
 
-        Display display = getWindowManager().getDefaultDisplay();
-        int screenWidth = display.getWidth();
+        mTitleIndicator = (SlidingTabLayout) findViewById(R.id.indicator);
+        mTitleIndicator.setViewPager(viewPager);
 
-        mTitleIndicator.setDividerWidth(screenWidth / 4);
-        mTitleIndicator.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+        mTitleIndicator.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+
             @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                mSelectedConfiguration = i;
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                mSelectedConfiguration = position;
                 setupMessage(true);
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
             }
         });
 
@@ -130,9 +133,7 @@ public class ScanActivity extends Activity implements CameraEventsListener, Scan
     private void setupMessage(boolean updateRecognizerView) {
         mMessage.setText(mConfiguration[mSelectedConfiguration].getTextResource());
 
-        mTitleAdapter.notifySelectedItemChanged();
-
-        mTitleIndicator.setSelection(mSelectedConfiguration);
+        mTitleIndicator.getViewPager().setCurrentItem(mSelectedConfiguration);
 
         if(updateRecognizerView) {
             BlinkOCRRecognizerSettings settings = new BlinkOCRRecognizerSettings();
@@ -195,6 +196,11 @@ public class ScanActivity extends Activity implements CameraEventsListener, Scan
         if (mRecognizerView != null && mRecognizerView.isCameraTorchSupported()) {
             mFlashButton.setVisibility(View.VISIBLE);
         }
+
+        // after camera is started, we can set the metering area for autofocus, white balance
+        // and auto exposure measurements
+        // we set the same rectangle as for scanning region
+        mRecognizerView.setMeteringAreas(new Rectangle[] {new Rectangle(0.1f, 0.34f, 0.8f, 0.13f)});
     }
 
     @Override
@@ -313,67 +319,10 @@ public class ScanActivity extends Activity implements CameraEventsListener, Scan
         setupMessage(true);
     }
 
-    /***** Title's list adapter *****/
-
-    private class ScanConfigurationListAdapter implements ListAdapter {
-
-        private Set<DataSetObserver> mObservers = new HashSet<DataSetObserver>();
+    private class SamplePagerAdapter extends PagerAdapter {
 
         /**
-         * Indicates whether all the items in this adapter are enabled. If the
-         * value returned by this method changes over time, there is no guarantee
-         * it will take effect.  If true, it means all items are selectable and
-         * clickable (there is no separator.)
-         *
-         * @return True if all items are enabled, false otherwise.
-         * @see #isEnabled(int)
-         */
-        @Override
-        public boolean areAllItemsEnabled() {
-            return true;
-        }
-
-        /**
-         * Returns true if the item at the specified position is not a separator.
-         * (A separator is a non-selectable, non-clickable item).
-         * <p/>
-         * The result is unspecified if position is invalid. An {@link ArrayIndexOutOfBoundsException}
-         * should be thrown in that case for fast failure.
-         *
-         * @param position Index of the item
-         * @return True if the item is not a separator
-         * @see #areAllItemsEnabled()
-         */
-        @Override
-        public boolean isEnabled(int position) {
-            return true;
-        }
-
-        /**
-         * Register an observer that is called when changes happen to the data used by this adapter.
-         *
-         * @param observer the object that gets notified when the data set changes.
-         */
-        @Override
-        public void registerDataSetObserver(DataSetObserver observer) {
-            mObservers.add(observer);
-        }
-
-        /**
-         * Unregister an observer that has previously been registered with this
-         * adapter via {@link #registerDataSetObserver}.
-         *
-         * @param observer the object to unregister.
-         */
-        @Override
-        public void unregisterDataSetObserver(DataSetObserver observer) {
-            mObservers.remove(observer);
-        }
-
-        /**
-         * How many items are in the data set represented by this Adapter.
-         *
-         * @return Count of items.
+         * @return the number of pages to display
          */
         @Override
         public int getCount() {
@@ -381,125 +330,42 @@ public class ScanActivity extends Activity implements CameraEventsListener, Scan
         }
 
         /**
-         * Get the data item associated with the specified position in the data set.
-         *
-         * @param position Position of the item whose data we want within the adapter's
-         *                 data set.
-         * @return The data at the specified position.
+         * @return true if the value returned from {@link #instantiateItem(ViewGroup, int)} is the
+         * same object as the {@link View} added to the {@link ViewPager}.
          */
         @Override
-        public Object getItem(int position) {
-            return mConfiguration[position];
+        public boolean isViewFromObject(View view, Object o) {
+            return o == view;
         }
 
         /**
-         * Get the row id associated with the specified position in the list.
-         *
-         * @param position The position of the item within the adapter's data set whose row id we want.
-         * @return The id of the item at the specified position.
-         */
-        @Override
-        public long getItemId(int position) {
-            return position;
-        }
-
-        /**
-         * Indicates whether the item ids are stable across changes to the
-         * underlying data.
-         *
-         * @return True if the same id always refers to the same object.
-         */
-        @Override
-        public boolean hasStableIds() {
-            return true;
-        }
-
-        /**
-         * Get a View that displays the data at the specified position in the data set. You can either
-         * create a View manually or inflate it from an XML layout file. When the View is inflated, the
-         * parent View (GridView, ListView...) will apply default layout parameters unless you use
-         * {@link android.view.LayoutInflater#inflate(int, android.view.ViewGroup, boolean)}
-         * to specify a root view and to prevent attachment to the root.
-         *
-         * @param position    The position of the item within the adapter's data set of the item whose view
-         *                    we want.
-         * @param convertView The old view to reuse, if possible. Note: You should check that this view
-         *                    is non-null and of an appropriate type before using. If it is not possible to convert
-         *                    this view to display the correct data, this method can create a new view.
-         *                    Heterogeneous lists can specify their number of view types, so that this View is
-         *                    always of the right type (see {@link #getViewTypeCount()} and
-         *                    {@link #getItemViewType(int)}).
-         * @param parent      The parent that this view will eventually be attached to
-         * @return A View corresponding to the data at the specified position.
-         */
-        @Override
-        public View getView(int position, View convertView, ViewGroup parent) {
-            if (convertView != null) {
-                setupView(position, convertView);
-                return convertView;
-            } else {
-                View v = getLayoutInflater().inflate(R.layout.page_element, null);
-                setupView(position, v);
-                return v;
-            }
-        }
-
-        private void setupView(int position, View v) {
-            TextView tv = (TextView) v.findViewById(R.id.txtTitle);
-            tv.setText(mConfiguration[position].getTitleResource());
-            tv.setTextColor(getResources().getColor((position == mSelectedConfiguration ? R.color.text_white : R.color.text_gray)));
-        }
-
-        /**
-         * Get the type of View that will be created by {@link #getView} for the specified item.
-         *
-         * @param position The position of the item within the adapter's data set whose view type we
-         *                 want.
-         * @return An integer representing the type of View. Two views should share the same type if one
-         * can be converted to the other in {@link #getView}. Note: Integers must be in the
-         * range 0 to {@link #getViewTypeCount} - 1. {@link #IGNORE_ITEM_VIEW_TYPE} can
-         * also be returned.
-         * @see #IGNORE_ITEM_VIEW_TYPE
-         */
-        @Override
-        public int getItemViewType(int position) {
-            return 0;
-        }
-
-        /**
+         * Return the title of the item at {@code position}. This is important as what this method
+         * returns is what is displayed in the {@link SlidingTabLayout}.
          * <p>
-         * Returns the number of types of Views that will be created by
-         * {@link #getView}. Each type represents a set of views that can be
-         * converted in {@link #getView}. If the adapter always returns the same
-         * type of View for all items, this method should return 1.
-         * </p>
-         * <p>
-         * This method will only be called when when the adapter is set on the
-         * the {@link AdapterView}.
-         * </p>
-         *
-         * @return The number of types of Views that will be created by this adapter
+         * Here we construct one using the position value, but for real application the title should
+         * refer to the item's contents.
          */
         @Override
-        public int getViewTypeCount() {
-            return 1;
+        public CharSequence getPageTitle(int position) {
+            return getResources().getString(mConfiguration[position].getTitleResource());
+        }
+        /**
+         * Instantiate the {@link View} which should be displayed at {@code position}. Here we
+         * inflate a layout from the apps resources and then change the text view to signify the position.
+         */
+        @Override
+        public Object instantiateItem(ViewGroup container, int position) {
+            return new View(ScanActivity.this);
         }
 
         /**
-         * @return true if this adapter doesn't contain any data.  This is used to determine
-         * whether the empty view should be displayed.  A typical implementation will return
-         * getCount() == 0 but since getCount() includes the headers and footers, specialized
-         * adapters might want a different behavior.
+         * Destroy the item from the {@link ViewPager}. In our case this is simply removing the
+         * {@link View}.
          */
         @Override
-        public boolean isEmpty() {
-            return mConfiguration.length == 0;
-        }
-
-        public void notifySelectedItemChanged() {
-            for(DataSetObserver dso : mObservers) {
-                dso.onChanged();
-            }
+        public void destroyItem(ViewGroup container, int position, Object object) {
+            container.removeView((View) object);
         }
     }
+
 }
