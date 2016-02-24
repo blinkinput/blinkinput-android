6f75170413e3bf3cb5c2c1a05e3e1e64b21ed1c2
Ivan Grce <ivan.grce@microblink.com>
2016-02-24 14:49:25 +0100

    Updated BlinkOCRDirectAPI demo app with examples for custom camera management in combination with BlinkOCR direct API

---
+++ BlinkOCRDemo/BlinkOCRDirectAPI/src/main/java/com/microblink/ocr/ExtrasKeys.java
@@ -0,0 +1,7 @@
+package com.microblink.ocr;
+
+public class ExtrasKeys {
+    public static final String EXTRAS_RECOGNITION_SETTINGS = "EXTRAS_RECOGNITION_SETTINGS";
+    public static final String EXTRAS_LICENSE_KEY = "EXTRAS_LICENSE_KEY";
+    public static final String EXTRAS_RECOGNITION_RESULTS = "EXTRAS_RECOGNITION_RESULTS";
+}
