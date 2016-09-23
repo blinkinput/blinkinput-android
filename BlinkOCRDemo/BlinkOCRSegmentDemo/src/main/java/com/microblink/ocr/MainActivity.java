5688e1574008ca769d4a22f89065e2195fbbc33f
Ivan Grce <ivan.grce@microblink.com>
2016-09-23 11:28:56 +0200

    Added mobile coupons parser example to BlinkOCRSegmentDemo app

--- BlinkOCRDemo/BlinkOCRSegmentDemo/src/main/java/com/microblink/ocr/MainActivity.java
+++ BlinkOCRDemo/BlinkOCRSegmentDemo/src/main/java/com/microblink/ocr/MainActivity.java
@@ -11,6 +11,7 @@ import com.microblink.help.HelpActivity;
 import com.microblink.recognizers.blinkocr.engine.BlinkOCREngineOptions;
 import com.microblink.recognizers.blinkocr.parser.generic.AmountParserSettings;
 import com.microblink.recognizers.blinkocr.parser.generic.IbanParserSettings;
+import com.microblink.recognizers.blinkocr.parser.mobilecoupons.MobileCouponsParserSettings;
 import com.microblink.recognizers.blinkocr.parser.regex.RegexParserSettings;
 import com.microblink.results.ocr.OcrFont;
 
@@ -20,13 +21,16 @@ public class MainActivity extends Activity {
     private static final int BLINK_OCR_REQUEST_CODE = 100;
     // obtain your licence key at http://microblink.com/login or
     // contact us at http://help.microblink.com
-    private static final String LICENSE_KEY = "CNDHGUQS-3REAUYG3-OJYH4FCG-QNW7QSOK-DEO5SIWW-MKYTEYZT-UGBW36CJ-YIELTPLQ";
+    private static final String LICENSE_KEY = "OE3CJCUH-JIRME5GT-YGVQ4IWW-MKYTEYZT-UGBW36CJ-ZIMR3WJC-2ZRLCEW4-INKK4C4M";
     private static final String NAME_TOTAL_AMOUNT = "TotalAmount";
     private static final String NAME_TAX = "Tax";
     private static final String NAME_IBAN = "IBAN";
 
     private static final int BLINK_OCR_VIN_REQUEST_CODE = 101;
     private static final String NAME_VIN = "VIN";
+
+    private static final int BLINK_OCR_MOBILE_COUPON_REQUEST_CODE = 102;
+    private static final String NAME_MOBILE_COUPON = "MobileCoupon";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -70,11 +74,11 @@ public class MainActivity extends Activity {
         intent.putExtra(SegmentScanActivity.EXTRAS_SCAN_CONFIGURATION, conf);
 
         // optionally, if we want the help screen to be available to user on camera screen,
-        // we can simply prepare an intent for help activity and pass it to BlinkOCRActivity
+        // we can simply prepare an intent for help activity and pass it to SegmentScanActivity
         Intent helpIntent = new Intent(this, HelpActivity.class);
         intent.putExtra(SegmentScanActivity.EXTRAS_HELP_INTENT, helpIntent);
 
-        // once intent is prepared, we start the BlinkOCRActivity which will preform scan and return results
+        // once intent is prepared, we start the SegmentScanActivity which will preform scan and return results
         // by calling onActivityResult
         startActivityForResult(intent, BLINK_OCR_REQUEST_CODE);
     }
@@ -84,7 +88,7 @@ public class MainActivity extends Activity {
      */
     public void regexExample(View v) {
         /*
-         * In this example we will use default BlinkOCRActivity to drive the recognition,
+         * In this example we will use default SegmentScanActivity to drive the recognition,
          * but here we will show how to setup a Regex parser. Regex parser allows configuring
          * custom regular expression which should be extracted from OCR result.
          *
@@ -93,7 +97,7 @@ public class MainActivity extends Activity {
          * of digits and uppercase letters.
          */
 
-        // same as in simple integration example, we will invoke scanning on BlinkOCRActivity,
+        // same as in simple integration example, we will invoke scanning on SegmentScanActivity,
         // so we need to setup an Intent for it.
         Intent intent = new Intent(this, SegmentScanActivity.class);
         // license key is required for recognizer to work.
@@ -121,13 +125,54 @@ public class MainActivity extends Activity {
         intent.putExtra(SegmentScanActivity.EXTRAS_SCAN_CONFIGURATION, conf);
 
         // optionally, if we want the help screen to be available to user on camera screen,
-        // we can simply prepare an intent for help activity and pass it to BlinkOCRActivity
+        // we can simply prepare an intent for help activity and pass it to SegmentScanActivity
         Intent helpIntent = new Intent(this, HelpActivity.class);
         intent.putExtra(SegmentScanActivity.EXTRAS_HELP_INTENT, helpIntent);
 
-        // once intent is prepared, we start the BlinkOCRActivity which will preform scan and return results
+        // once intent is prepared, we start the SegmentScanActivity which will preform scan and return results
         // by calling onActivityResult
         startActivityForResult(intent, BLINK_OCR_VIN_REQUEST_CODE);
+    }
+
+    /**
+     * Called as handler for "Mobile coupons example" button
+     */
+    public void mobileCouponsExample(View view) {
+        /*
+         * In this simple example we will use BlinkOCR SDK to create a simple app
+         * that scans prepaid mobile coupon codes.
+         */
+
+        Intent intent = new Intent(this, SegmentScanActivity.class);
+        // license key is required for recognizer to work.
+        intent.putExtra(SegmentScanActivity.EXTRAS_LICENSE_KEY, LICENSE_KEY);
+
+        // create settings for mobile coupon parser, find codes with prefix "*123*"
+        MobileCouponsParserSettings couponParserSett = new MobileCouponsParserSettings(MobileCouponsParserSettings.TopUpPrefix.TOP_UP_PREFIX_123);
+        // it is possible to create parser settings with given prefix and USSD length for codes in form *prefixString*USSDCodeLength_digits#
+        // MobileCouponsParserSettings couponParserSett = new MobileCouponsParserSettings("01", 14);
+
+        // allow codes without prefix, but prefix "*123*" will be added (we defined prefix in parser settings constructor)
+        couponParserSett.setAllowNoPrefix(true);
+
+        // we need to scan 1 item, so we will add 1 scan configuration to scan configuration array
+        ScanConfiguration conf[] = new ScanConfiguration[] {
+                // each scan configuration contains two string resource IDs: string shown in title bar and string shown
+                // in text field above scan box. Besides that, it contains name of the result and settings object
+                // which defines what will be scanned.
+                new ScanConfiguration(R.string.mobile_coupon_title, R.string.mobile_coupon_msg, NAME_MOBILE_COUPON, couponParserSett)
+        };
+
+        intent.putExtra(SegmentScanActivity.EXTRAS_SCAN_CONFIGURATION, conf);
+
+        // optionally, if we want the help screen to be available to user on camera screen,
+        // we can simply prepare an intent for help activity and pass it to SegmentScanActivity
+        Intent helpIntent = new Intent(this, HelpActivity.class);
+        intent.putExtra(SegmentScanActivity.EXTRAS_HELP_INTENT, helpIntent);
+
+        // once intent is prepared, we start the SegmentScanActivity which will preform scan and return results
+        // by calling onActivityResult
+        startActivityForResult(intent, BLINK_OCR_MOBILE_COUPON_REQUEST_CODE);
     }
 
     /**
@@ -137,26 +182,27 @@ public class MainActivity extends Activity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        // first we need to check that we have indeed returned from BlinkOCRActivity with
-        // success
-        if(requestCode == BLINK_OCR_REQUEST_CODE && resultCode == SegmentScanActivity.RESULT_OK) {
+        // first we need to check that we have indeed returned from SegmentScanActivity with success
+        if (resultCode == SegmentScanActivity.RESULT_OK) {
             // now we can obtain bundle with scan results
             Bundle result = data.getBundleExtra(SegmentScanActivity.EXTRAS_SCAN_RESULTS);
-
-            // each result is stored under key equal to the name of the scan configuration that generated it
-            String totalAmount = result.getString(NAME_TOTAL_AMOUNT);
-            String taxAmount = result.getString(NAME_TAX);
-            String iban = result.getString(NAME_IBAN);
-
-            Toast.makeText(this, "To IBAN: " + iban + " we will pay total " + totalAmount + ", tax: " + taxAmount, Toast.LENGTH_LONG).show();
-        } else if (requestCode == BLINK_OCR_VIN_REQUEST_CODE && resultCode == SegmentScanActivity.RESULT_OK) {
-            // now we can obtain bundle with scan results
-            Bundle result = data.getBundleExtra(SegmentScanActivity.EXTRAS_SCAN_RESULTS);
-
-            // each result is stored under key equal to the name of the scan configuration that generated it
-            String vin = result.getString(NAME_VIN);
-
-            Toast.makeText(this, "Vehicle identification number is: " + vin, Toast.LENGTH_LONG).show();
+            switch (requestCode) {
+                case BLINK_OCR_REQUEST_CODE:
+                    // each result is stored under key equal to the name of the scan configuration that generated it
+                    String totalAmount = result.getString(NAME_TOTAL_AMOUNT);
+                    String taxAmount = result.getString(NAME_TAX);
+                    String iban = result.getString(NAME_IBAN);
+                    Toast.makeText(this, "To IBAN: " + iban + " we will pay total " + totalAmount + ", tax: " + taxAmount, Toast.LENGTH_LONG).show();
+                    break;
+                case BLINK_OCR_VIN_REQUEST_CODE:
+                    String vin = result.getString(NAME_VIN);
+                    Toast.makeText(this, "Vehicle identification number is: " + vin, Toast.LENGTH_LONG).show();
+                    break;
+                case BLINK_OCR_MOBILE_COUPON_REQUEST_CODE:
+                    String mobileCouponCode = result.getString(NAME_MOBILE_COUPON);
+                    Toast.makeText(this, "Mobile coupon code is: " + mobileCouponCode, Toast.LENGTH_LONG).show();
+                    break;
+            }
         }
     }
 }
